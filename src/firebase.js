8393e2a1e0b57917147b81bde848d69b2924f2f9
Prince Jr <mprincegame@gmail.com>
2026-06-03 12:10:37 +0100

    First Official Release

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ import { getFirestore } from 'firebase/firestore';
 // Configuration Firebase de SmartBay
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUT_DOMAIN,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
